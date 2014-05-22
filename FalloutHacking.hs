35568cae750be821569578dba4659e6a38935926
Travis Poulsen <travis.poulsen@gmail.com>
2014-05-22 11:49:25 -0400

    Refactoring wordlist read time

--- FalloutHacking.hs
+++ FalloutHacking.hs
@@ -16,19 +16,19 @@ type WordNumber   = Int
 wordLengthQuant :: StdGen -> Difficulty -> (WordLength, WordNumber)
 wordLengthQuant g d = combos !! fst (randomR (0, length combos - 1) g)
   where combos = case d of
-                  1 -> possibleCombos [4..6]
-                  2 -> possibleCombos [6..8]
-                  3 -> possibleCombos [8..10]
-                  4 -> possibleCombos [10..12]
-                  5 -> possibleCombos [12..15]
-                  otherwise -> possibleCombos [4..15]
+            1 -> possibleCombos [4..6]
+            2 -> possibleCombos [6..8]
+            3 -> possibleCombos [8..10]
+            4 -> possibleCombos [10..12]
+            5 -> possibleCombos [12..15]
+            otherwise -> possibleCombos [4..15]
         possibleCombos z = (\x y -> (x,y)) <$> z <*> z
 
 readWordList :: FilePath -> IO WordList
 readWordList x = do
   corpus <- readFile x
-  return . validWords . map T.pack . words $ corpus
-    where validWords = filter (\y -> T.length y <= 15 && T.length y >= 4)
+  return . map T.pack . words $ corpus
+    --where validWords = filter (\y -> T.length y <= 15 && T.length y >= 4)
 
 getRandomWords :: WordList -> (WordLength, WordNumber) -> StdGen -> WordList
 getRandomWords w (l,n) g = map (\x -> useableWords !! x) randomNs
@@ -40,21 +40,21 @@ checkGuess :: GoalWord -> GuessWord -> CorrectLetters
 checkGuess goal guess = foldr (\(x,y) -> if x == y then (+1) else (+0)) 0 $ T.zip goal guess
 
 main = do
-  g     <- getStdGen
+  g         <- getStdGen
   gameState <- setUpGame g
-  result  <- gameLoop gameState
+  result    <- gameLoop gameState
   putStrLn "GAME OVER"
   
 setUpGame :: StdGen -> IO GameState
 setUpGame g = do
-  rawWordList <- readWordList "enable1.txt"
   difficulty  <- putStr "Difficulty (1-5)? " >> hFlush stdout >> readLn :: IO Int
   if difficulty `elem` [1..5]
   then do
+    rawWordList    <- readWordList "enable1.txt"
     let wordLenNum = wordLengthQuant g difficulty
     let wordList   = map T.toUpper $ getRandomWords rawWordList wordLenNum g 
-    goalIndex <- randomRIO (0, length wordList - 1)
-    let gameState = (0, goalWord, wordList)
+    goalIndex      <- randomRIO (0, length wordList - 1)
+    let gameState  = (0, goalWord, wordList)
           where goalWord = wordList !! goalIndex
     mapM_ (putStrLn . T.unpack) wordList 
     return gameState
