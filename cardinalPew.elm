9fc5d3aaf6c392dc412832862942a1a00dc00d59
Travis Poulsen <travis.poulsen@gmail.com>
2014-08-11 13:50:48 -0400

    Implemented weapon cooldown

--- cardinalPew.elm
+++ cardinalPew.elm
@@ -10,12 +10,12 @@ listToMaybe x = if | x == []   -> Nothing
 
 
 ------ INPUTS ------
-type Input = { yDir:Int, fire:Bool, shift:Bool, swap:Bool, delta:Time }
+type Input = { yDir:Int, fire:(Time, Bool), shift:Bool, swap:Bool, delta:Time }
 
 delta = lift (\t -> t/10) (fps 60)
 
 input = sampleOn delta (Input <~ lift .y Keyboard.arrows
-                               ~ Keyboard.space
+                               ~ Time.timestamp Keyboard.space
                                ~ Keyboard.shift
                                ~ Keyboard.isDown (Char.toCode 'C')
                                ~ delta)
@@ -27,7 +27,7 @@ type Game p e w   = { player:(Player p w), enemies:[(Enemy e)], shots:[Projectil
 type Player p w   = { p | x:Float, y:Float, vy:Float, firing:Bool, weapons:[(Weapon w)], boost:Bool, health:Int }
 type Enemy  e     = { e | x:Float, y:Float, vy:Float, sides:Int, radius:Float, alive:Bool  }
 type Weapon w     = { w | kind:WeaponTypes, active:Bool, cooldown:Time }
-type Projectiles  = { kind:WeaponTypes, x:Float, y:Float, vx:Float, vy:Float, alive:Bool, radius:Float }
+type Projectiles  = { kind:WeaponTypes, x:Float, y:Float, vx:Float, vy:Float, alive:Bool, radius:Float, fired:Time }
 type GameObject o = { o | vx:Float} 
 
 --defaultGame : Game
@@ -35,10 +35,13 @@ defaultGame = { player=player1, enemies=[enemy1, enemy2], shots=[], state=Playin
 
 --player1 : Player (GameObject {})
 player1 = { x=-halfWidth+padding, y=0, vx=0, vy=0, firing=False, weapons=[weapon1, weapon2], health=100, boost=False }
-weapon1 = { kind=Blaster, active=True,   cooldown=(second), x=0, y=0, vx=0, alive=False }
-weapon2 = { kind=WaveBeam, active=False, cooldown=(second), x=0, y=0, vx=0, alive=False }
+weapon1 = { kind=Blaster, active=True,   cooldown=(50 * millisecond), x=0, y=0, vx=0, alive=False }
+weapon2 = { kind=WaveBeam, active=False, cooldown=(millisecond), x=0, y=0, vx=0, alive=False }
 enemy1 =  { x=halfWidth+100, y=0, vx=-1, vy=0, sides=5, radius=30, alive=True }
 enemy2 =  { x=halfWidth, y=100, vx=-1, vy=0, sides=7, radius=15, alive=True }
+
+--ENEMY GENERATOR
+
 
 ------ UPDATE ------
 collision o1 o2 = (o1.x >= o2.x - o2.radius && o1.x <= o2.x + o2.radius) &&
@@ -50,7 +53,7 @@ moveP yDir boost p = if p.health > 0
                      else p
 
 --Objects need to constantly move <-; Relies on time.
-moveO t n o = { o | vx <- n*t/10   }
+moveO t n o = { o | vx <- n*t/10     }
 
 physics t p = { p | y <- p.y + t * p.vy * 2 |> clamp (-halfHeight) (halfHeight)
                   , x <- p.x + t * p.vx * 10 }
@@ -69,13 +72,14 @@ swapWeapons i p = { p | weapons <- if | i.swap -> map (\x -> { x | active <- not
        
 shoot i s p = 
     let w = activeWeapon p.weapons
-    in if i.fire 
+        f = s |> listToMaybe |> maybe {kind=Blaster, x=halfWidth, y=0, vy=0, vx=3.0, radius=4.0, alive=True, fired=i.delta} (\x -> x)
+    in if (snd i.fire) && (fst i.fire) - f.fired >= w.cooldown 
        then { kind=w.kind, x=p.x+17, y=p.y, vy=0,
-              vx=if | w.kind == Blaster  -> 3.0
-                    | w.kind == WaveBeam -> 5.0 
+              vx=     if | w.kind == Blaster  -> 3.0
+                         | w.kind == WaveBeam -> 5.0 
             , radius= if | w.kind == Blaster  -> 4.0
                          | w.kind == WaveBeam -> 10.0 
-            , alive=True } :: s
+            , alive=True, fired=(fst i.fire) } :: s
        else s 
 
 shotsHit s e = if any (\shot -> collision shot e) s then { e | alive <-  False }
@@ -111,9 +115,9 @@ make p = ngon 3 20 |> outlined (solid white)
                    |> move (p.x, p.y)
 
 makeShots s = 
-    if | s.kind == Blaster  -> circle 4   |> filled red
+    if | s.kind == Blaster  -> circle 4   |> outlined (solid red)
                                           |> move (s.x, s.y)
-       | s.kind == WaveBeam -> rect 15 10 |> filled purple
+       | s.kind == WaveBeam -> rect 15 10 |> outlined (solid purple)
                                           |> move (s.x, s.y)
        | otherwise -> rect 5 20 |> filled lightBlue |> move (s.x, s.y)
 
